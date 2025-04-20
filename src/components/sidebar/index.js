f8837f562b4a9c6b6da1a63afc923048cc4a3030
Ritika Lama <ritika0815@MacBookAir.ht.home>
2025-04-20 13:59:06 -0500

    Added some buttons in the side bar and improvised icon

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import './sidebar.css'
 import SidebarButton from './sidebarButton'
-import { FcLikePlaceholder } from "react-icons/fc";
+import { IoIosHeart } from "react-icons/io";
+import { MdLibraryMusic } from "react-icons/md";
+import { RiMusicAiFill } from "react-icons/ri";
+import { FaFireAlt } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
+import { VscSignOut } from "react-icons/vsc";
+
 
 
 export default function Sidebar() {
   return (
     <div className='sidebar-container'>
-      <img src='./public/profile.jpg' alt='profile' className='profile-img' />
+      <img src={`${process.env.PUBLIC_URL}/profile.jpg`} alt="profile" className="profile-img" />
       <div>
-        < SidebarButton title="Favorites" to="/favorites" icon={FcLikePlaceholder}/>
-        < SidebarButton/>
-        < SidebarButton/>
-        < SidebarButton/>
+        < SidebarButton title="Favorites" to="/favorites" icon={<IoIosHeart/>}/>
+        < SidebarButton title="Library" to="/library" icon={<MdLibraryMusic/>}/>
+        < SidebarButton title="Player" to="/player" icon={<RiMusicAiFill/>}/>
+        < SidebarButton title="Trending" to="/trending" icon={<FaFireAlt/>}/>
+        < SidebarButton title="Feed" to="/feed" icon={<FaHome/>}/>
       </div>
-      < SidebarButton/>
+      < SidebarButton title="Sign Out" to="" icon={<VscSignOut/>}/>
     </div>
   )
 }
