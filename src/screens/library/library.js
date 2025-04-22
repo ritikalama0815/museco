e3dc9e5e32089f023c26ce6705cbc9a1bebf28fd
Ritika Lama <ritikatheeng2002@gmail.com>
2025-04-21 23:33:49 -0500

    modified home and lib

--- src/screens/library/library.js
+++ src/screens/library/library.js
@@ -1,7 +1,11 @@
 import React from 'react'
+// import APIKit from '../../spotify'
 
 
 export default function Library() {
+  // APIKit.get('me/playlists').then(function(response){
+  //   console.log(response.data)
+  // })
   return (
     <div className='screen-container'>
       Library
